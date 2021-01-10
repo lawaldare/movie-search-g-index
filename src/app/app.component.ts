cc871be1bafe1d03399b9c25039d04b5da877419
= <=>
2021-01-10 17:33:59 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tailwindcssapp';
+  movieName: string;
+  prefixForSearch: string = "https://www.google.com/search?q=intitle:index.of? mkv ";
+
+
+  search(form: NgForm) {
+    let movie = form.value.movieName;
+    window.open(`${this.prefixForSearch}${movie}`);
+    form.resetForm();
+  }
 }
